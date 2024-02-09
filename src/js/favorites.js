ceaf4dec3a2e99b5387c6ad7f56da392289a439d
OksanaBakhaieva <obahaeva@gmail.com>
2024-02-09 00:54:55 +0000

    Update function createMarkup in favorites.js

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,8 +1,11 @@
+
+
+
+
 function createMarkupFavorites(hits) { 
   return hits
     .map(
         ({
-            _id,
         bodyPart,
         name,
         target,
@@ -10,27 +13,20 @@ function createMarkupFavorites(hits) {
         time,
         }) =>
           `
-          <ul class=""favorites-gallery">
-            < class="favorites-gallery-item">
-               <h4 class="workout">WORKOUT</h4>
-               <a class="favorites-remove" href="#"> <img class="favorites-icon-bin" src="#" alt="icon-bin"/></a>
-                <a class="favorites-start" href="#">Start 
-                    <span>
-                    <img class="favorites-icon-arrow" src="#" alt="icon-arrow"/> 
-                    </span>  
-                </a>
-               <img class="favorites-icon-runner" src="#" alt="icon-runner"/>
-            <h3>${name}</h3>
-            <ul class="favorites-gallery-info">
-              <li class="favorites-gallery-info-item">Burned calories: <span class="descr-span">${burnedCalories} / ${time} min</span></li>
-              <li class="favorites-gallery-info-item">Body part: <span class="descr-span">${bodyPart}</span></li>
-              <li class="favorites-gallery-info-item">Target: <span class="descr-span">${target}</span></li>
-            </ul> 
-          <a class="favorites-gallery-link" href="#">Start 
-            <span>
-              <img class="favorites-icon-arrow" src="#" alt="icon-arrow"/> 
-            </span> 
-          </a>`
+          <ul class="favorites-gallery">
+            <li class="favorites-gallery-item">
+                <span class="workout">WORKOUT</span>
+                <a class="favorites-remove" href="#"> <img class="favorites-remove-icon" src="../img/icons/all icons/basket.svg" alt="remove-icon"/></a>
+                <a class="favorites-start" href="#">Start <img class="favorites-start-icon" src="../img/icons/all icons/line.svg" alt="start-icon"/></a>
+                <img class="favorites-man-icon" src="../img/icons/all icons/Man.svg" alt="man-icon"/>
+                <h3 class="favorites-item-title">${name}</h3>
+                <ul class="favorites-gallery-info">
+                  <li class="favorites-gallery-info-item">Burned calories: <span class="descr-span">${burnedCalories} / ${time} min</span></li>
+                  <li class="favorites-gallery-info-item">Body part: <span class="descr-span">${bodyPart}</span></li>
+                  <li class="favorites-gallery-info-item">Target: <span class="descr-span">${target}</span></li>
+                </ul> 
+            </li>
+          </ul>`
       )
         .join('');
     
