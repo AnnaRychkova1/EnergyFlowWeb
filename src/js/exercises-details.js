825d9f1f78138992f358f802b14e10077bfb3bdf
AnnaRychkova1 <aanytkaa@gmail.com>
2024-02-09 22:31:28 +0000

    refs

--- src/js/exercises-details.js
+++ src/js/exercises-details.js
@@ -33,13 +33,13 @@ const queryParams = {
 // }
 
 
-const refs = {
-  exercisesHeader: document.querySelector('.exersises-header'),
-  resultContainer: document.querySelector('.filtered-cards'),
-  searchForm: document.querySelector('.form'),
-  searchBtn: document.querySelector('.search-btn'),
-  textResult: document.querySelector('.exercise-text-no-found'),
-}
+// const refs = {
+//   exercisesHeader: document.querySelector('.exersises-header'),
+//   resultContainer: document.querySelector('.filtered-cards'),
+//   searchForm: document.querySelector('.form'),
+//   searchBtn: document.querySelector('.search-btn'),
+//   textResult: document.querySelector('.exercise-text-no-found'),
+// }
 
 
 refs.exercisesHeader.textContent = `/${nameExercise}`;
