188533a96e12ba9170914a66e8bf44691d4f0a01
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-02-09 21:05:49 +0200

    Correction

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,67 +1,110 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import axios from 'axios';
 
 const BASE_URL = 'https://energyflow.b.goit.study/api';
 const ENDPOINT = 'filters';
 
-const exercisesBtnEl = document.querySelector('.exercises-btn');
+const exercisesBtnEl = document.querySelector('.exercises-btn-list');
 const exercisesGalleryEl = document.querySelector('.exercises-gallery');
+const paginationEl = document.querySelector('.pagination');
 
-let page = 1;
+let filterDefault = 'Muscles';
+let currentPage = 1;
+let currentLimit = 0;
+let screenWidth = window.innerWidth;
 
-async function getExercisesByMuscles() {
+if (screenWidth <= 375) {
+  currentLimit = 8;
+} else if (screenWidth <= 768) {
+  currentLimit = 12;
+} else {
+  currentLimit = 12;
+}
+
+exercisesBtnEl.addEventListener('click', filterBtnExercises);
+
+async function getExercisesByFilter() {
   try {
     const response = await axios.get(`${BASE_URL}/${ENDPOINT}`, {
       params: {
-        filter: 'Muscles',
-        page: page,
-        limit: 12,
+        filter: filterDefault,
+        page: currentPage,
+        limit: currentLimit,
       },
     });
-    // return response;
-    console.log(response);
-    exercisesGalleryEl.innerHTML = createExercisesByMusclesMarkup(
-      response.data.results
-    );
+    return response.data;
   } catch (error) {
     console.error(error);
   }
 }
 
-getExercisesByMuscles();
+async function filterBtnExercises(event) {
+  event.preventDefault();
+  const query = event.target.dataset.filter;
+  exercisesGalleryEl.innerHTML = '';
+  if (!query) {
+    return;
+  }
+  filterDefault = query;
+  currentPage = 1;
+  try {
+    getExercisesByFilter(query).then(data => {
+      const { results, page, totalPages } = data;
+      createExercisesByFilterMarkup(results);
+      if (totalPages > 1) {
+        const moveButton = pagesPagination(page, totalPages);
+        paginationEl.innerHTML = moveButton;
+      } else {
+        paginationEl.innerHTML = '';
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const createExercisesByMusclesMarkup = arr =>
-  arr
+// background: linear-gradient(0deg, rgba(16, 16, 16, 0.70) 0%, rgba(16, 16, 16, 0.70) 100%), url(${imgUrl});
+//          background-size: cover;
+// background-repeat: no-repeat;"
+
+function createExercisesByFilterMarkup(results) {
+  const markup = results
     .map(
       ({ name, filter, imgUrl }) =>
-        `<li class="exercises-gallery-item"
-         style="background: linear-gradient(0deg, rgba(16, 16, 16, 0.70) 0%, rgba(16, 16, 16, 0.70) 100%), url(${imgUrl});
-         background-size: cover;
-background-repeat: no-repeat;">
-<div class="exercises-gallery-text">
+        `<li class="exercises-gallery-item" data-filter>
+        <img class="exercises-gallery-img" src="${imgUrl}" alt="${filter}">
+        <div class="exercises-gallery-text">
           <h3 class="exercises-gallery-title">${name}</h3>
           <p class="exercises-gallery-filter">${filter}</p>
-          </div> 
-      </li>`
+        </div>
+        </li>`
     )
     .join('');
+  exercisesGalleryEl.insertAdjacentHTML('beforeend', markup);
+}
+function pagesPagination(page, totalPages) {
+  let disabledMoveButton = '';
+  for (let i = 1; i <= totalPages; i += 1) {
+    disabledMoveButton += `<button class="pagination-btn" type="button">${i}</button>`;
+  }
+  return disabledMoveButton;
+}
 
-// function onFetchError(error) {
-//   iziToast.error({
-//     title: 'Error',
-//     message:
-//       'Sorry, there are no images matching your search query. Please try again!',
-//     position: 'topRight',
-//     messageColor: '#fff',
-//     messageSize: '20px',
-//     backgroundColor: '#EF4040',
-//     close: false,
-//     closeOnClick: true,
-//     progressBarEasing: 'linear',
-//   });
-// }
+async function onPaginationPages(event) {
+  currentPage = event.target.textContent;
+  exercisesGalleryEl.innerHTML = '';
+  try {
+    const { results, page, totalPages } = await getExercisesByFilter();
+    const filter = results[0].filter;
+
+    if (page === totalPages) {
+      return;
+    }
+    createExercisesByFilterMarkup(results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+paginationEl.addEventListener('click', onPaginationPages);
