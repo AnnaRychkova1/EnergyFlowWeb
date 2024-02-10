d1f67a16f2ec9b7744e34e673c13c3fa8def5cc0
AnnaRychkova1 <aanytkaa@gmail.com>
2024-02-10 09:52:02 +0000

    Update tui-pagination.js

--- src/js/services/tui-pagination.js
+++ src/js/services/tui-pagination.js
@@ -1,8 +1,8 @@
-import Pagination from 'tui-pagination';
-import 'tui.pagination/dist/tui-pagination.min.css';
+// import Pagination from 'tui-pagination';
+// import 'tui.pagination/dist/tui-pagination.min.css';
 
-  export const pagination1 = new tui.Pagination('pagination1', {
-        totalItems: 500,
-        itemsPerPage: 10,
-        visiblePages: 5
-    });
+  // export const pagination1 = new tui.Pagination('pagination1', {
+  //       totalItems: 500,
+  //       itemsPerPage: 10,
+  //       visiblePages: 5
+  //   });
