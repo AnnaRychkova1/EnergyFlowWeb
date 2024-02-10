bc9165a5804c2f59c12f7f8c8f842188cac332ac
AnnaRychkova1 <aanytkaa@gmail.com>
2024-02-10 09:08:55 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,4 +7,10 @@ import('./js/exercises-details.js');
 import('./js/footer.js');
 import('./js/favorites.js');
 import('./js/modal-menu.js');
-
+import('./js/templates/localStorage.js');
+import('./js/templates/refs.js');
+import('./js/services/isiToast.js');
+import('./js/services/mainApi.js');
+import('./js/services/notify.js');
+import('./js/services/tui-pagination.js');
+import('./js/services/visibility.js');
