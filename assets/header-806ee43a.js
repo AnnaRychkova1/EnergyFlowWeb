7c1401f3bed011d96e7f0b3d2f4ba679818ee010
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-12 20:50:35 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@c9345e498f981286ffae2c0ff1b6901c3a23e0bd 🚀

---
+++ assets/header-806ee43a.js
@@ -0,0 +1,2 @@
+let r=".navigation-links",i=location.pathname;const t=document.querySelectorAll(r);let l="";i.includes("favorites")?l="favorites":l="home";for(let e=0;e<t.length;e++){let a=t[e].getAttribute("data-href");l==a&&t[e].classList.add("current")}
+//# sourceMappingURL=header-806ee43a.js.map
--- assets/header-8e0248cc.js
+++
@@ -1,2 +0,0 @@
-let n=".navigation-links",i=location.pathname;const l=document.querySelectorAll(n);console.log(l);for(let e=0;e<l.length;e++){let t=l[e].getAttribute("href");i.includes(t)&&l[e].classList.add("current")}
-//# sourceMappingURL=header-8e0248cc.js.map
