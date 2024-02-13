9b97144a17c3b2c863a3f9551eb9852dbfb98d55
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-13 04:52:59 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@be8e35b497e77dc7a0d0978f6b98665348d06255 🚀

--- assets/notify-804b4084.js
+++
@@ -1,2 +0,0 @@
-import{n as t}from"./vendor-65c0bc71.js";function o(){t.Notify.success("Peace at home, peace in the world.",{ID:"MKA",timeout:1923,showOnlyTheLastOne:!0})}const i={apiNotifySucces:o};export{i as default};
-//# sourceMappingURL=notify-804b4084.js.map
