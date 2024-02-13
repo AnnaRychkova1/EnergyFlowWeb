16246d130c40b832f272ac9b6130960b8ab082aa
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-13 05:27:22 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@19ddd6c558c3961973ef8f8895dccd37ddde012d 🚀

---
+++ assets/favorites-754f9214.js
@@ -0,0 +1,2 @@
+import"./refs-326154d5.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
+//# sourceMappingURL=favorites-754f9214.js.map
--- assets/hero-4ed993c7.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=hero-4ed993c7.js.map
