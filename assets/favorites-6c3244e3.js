1bc97fcdf5047915439b1eb35db0f3e7355fdac8
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-02-13 14:52:31 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@7487ea76c51fd664cad81c9c56d34f0b54a0d120 🚀

---
+++ assets/favorites-6c3244e3.js
@@ -0,0 +1,2 @@
+import"./refs-56e9387c.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
+//# sourceMappingURL=favorites-6c3244e3.js.map
--- assets/favorites-754f9214.js
+++
@@ -1,2 +0,0 @@
-import"./refs-326154d5.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
-//# sourceMappingURL=favorites-754f9214.js.map
