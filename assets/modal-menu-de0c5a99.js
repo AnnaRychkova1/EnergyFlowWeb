8f18e5461a2be46b5c6e3a8079a550aae9525fdb
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-13 04:12:13 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@02849ce2e9dd4870a2d3fb6ea9eccc2418982767 🚀

--- assets/modal-menu-de0c5a99.js
+++ assets/modal-menu-de0c5a99.js
@@ -1,4 +1,4 @@
-import{a as b}from"./vendor-65c0bc71.js";const m="/EnergyFlowWeb/assets/symbol-defs-99a4d0de.svg";document.querySelector(".results");const i=document.querySelector(".backdrop"),c=document.querySelector(".modal");document.querySelector(".ex-add-favorite");const L="64f389465ae26083f39b17c0";I();let k="favorites",r=localStorage.getItem(k);r?r=JSON.parse(r):r=[];const S=document.querySelector(".ex-add-favorite");S.addEventListener("click",w);const $=document.querySelector(".modal-close-btn");$.addEventListener("click",E);i.addEventListener("click",o);document.addEventListener("keydown",d);async function w(e){const s=e.target.closest(".ex-add-favorite");if(!s)return;const t=s.dataset.id,a=JSON.parse(localStorage.getItem("favorites"))||[];if(a.some(n=>n._id===t)){const n=a.filter(u=>u._id!==t);localStorage.setItem("favorites",JSON.stringify(n)),s.classList.remove("is-favorite"),s.innerHTML=changingButtonName("add")}else{const n=await f(t);localStorage.setItem("favorites",JSON.stringify([...a,n])),s.classList.add("is-favorite"),s.innerHTML=changingButtonName("remove")}}function E(){c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o)}function o(e){e.target.closest(".modal")||(c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o))}function d(e){e.preventDefault(),e.key==="Escape"&&(c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o))}async function f(e){try{const s="https://energyflow.b.goit.study/api",t="exercises",{data:a}=await b.get(`${s}/${t}/${e}`);return a}catch(s){console.error(s)}}async function I(){try{const{results:e}=await f(L);M(e),console.log(e)}catch(e){console.error(e.message)}finally{}}function M(e={}){const{_id:s,bodyPart:t,equipment:a,time:l,target:n,burnedCalories:u,gifUrl:g,name:v,popularity:p,rating:h,description:x}=e,y=`<div class="modal">
+import{a as b}from"./vendor-65c0bc71.js";const m="/EnergyFlowWeb/assets/symbol-defs-99a4d0de.svg",L="https://energyflow.b.goit.study/api",k="exercises";document.querySelector(".results");const i=document.querySelector(".backdrop"),c=document.querySelector(".modal");document.querySelector(".ex-add-favorite");const S="64f389465ae26083f39b17c0";T();let E="favorites",r=localStorage.getItem(E);r?r=JSON.parse(r):r=[];const $=document.querySelector(".ex-add-favorite");$.addEventListener("click",I);const w=document.querySelector(".modal-close-btn");w.addEventListener("click",M);i.addEventListener("click",o);document.addEventListener("keydown",d);async function I(e){const s=e.target.closest(".ex-add-favorite");if(!s)return;const a=s.dataset.id,n=JSON.parse(localStorage.getItem("favorites"))||[];if(n.some(t=>t._id===a)){const t=n.filter(u=>u._id!==a);localStorage.setItem("favorites",JSON.stringify(t)),s.classList.remove("is-favorite"),s.innerHTML=changingButtonName("add")}else{const t=await f(a);localStorage.setItem("favorites",JSON.stringify([...n,t])),s.classList.add("is-favorite"),s.innerHTML=changingButtonName("remove")}}function M(){c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o)}function o(e){e.target.closest(".modal")||(c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o))}function d(e){e.preventDefault(),e.key==="Escape"&&(c.classList.add("is-hidden"),i.classList.add("is-hidden"),c.innerHTML="",document.removeEventListener("keydown",d),i.removeEventListener("click",o))}async function f(e){try{const{data:s}=await b.get(`${L}/${k}/${e}`);return s}catch(s){console.error(s)}}async function T(){try{const{results:e}=await f(S);C(e),console.log(e)}catch(e){console.error(e.message)}finally{}}function C(e={}){const{_id:s,bodyPart:a,equipment:n,time:l,target:t,burnedCalories:u,gifUrl:g,name:v,popularity:p,rating:h,description:x}=e,y=`<div class="modal">
             <button class="modal-close-btn">
                 <svg
                     class="modal-close-icon"
@@ -34,21 +34,21 @@ import{a as b}from"./vendor-65c0bc71.js";const m="/EnergyFlowWeb/assets/symbol-d
                 <h3 class="exercise-name">${v}</h3>
                 <p class="ex-current-rating">${h}</p>
                 <ul class="exercise-stars-list">
-                    ${T(p)}
+                    ${q(p)}
                 </ul>
 
                 <div class="exercise-information">
                     <div class="ex-block">
                         <span class="exercise-value">Target</span>
-                        <span class="exercise-label">${n}</span>
-                    </div>
-                    <div class="ex-block">
-                        <span class="exercise-value">Body part</span>
                         <span class="exercise-label">${t}</span>
                     </div>
                     <div class="ex-block">
-                        <span class="exercise-value">Equipment</span>
+                        <span class="exercise-value">Body part</span>
                         <span class="exercise-label">${a}</span>
+                    </div>
+                    <div class="ex-block">
+                        <span class="exercise-value">Equipment</span>
+                        <span class="exercise-label">${n}</span>
                     </div>
                     <div class="ex-block">
                         <span class="exercise-value">Popular</span>
@@ -80,5 +80,5 @@ import{a as b}from"./vendor-65c0bc71.js";const m="/EnergyFlowWeb/assets/symbol-d
                 </div>
             </div>
         </div>
-    `;return c.innerHTML=y}function T(e){const s='<span class="star">&#9733;</span>',t='<span class="star">&#9734;</span>',a=s.repeat(Math.floor(e)),l=e%1!==0?'<span class="star">&#9733;</span>':"",n=t.repeat(5-Math.ceil(e));return a+l+n}export{f as getCardInfo,I as renderCard};
-//# sourceMappingURL=modal-menu-4e6e9d9a.js.map
+    `;return c.innerHTML=y}function q(e){const s='<span class="star">&#9733;</span>',a='<span class="star">&#9734;</span>',n=s.repeat(Math.floor(e)),l=e%1!==0?'<span class="star">&#9733;</span>':"",t=a.repeat(5-Math.ceil(e));return n+l+t}export{f as getCardInfo,T as renderCard};
+//# sourceMappingURL=modal-menu-de0c5a99.js.map
