fb00353c332be1485890721009bd54fbf0f19e54
OksanaBakhaieva <142331458+OksanaBakhaieva@users.noreply.github.com>
2024-02-15 07:16:03 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@936dd1309e4f57627c20ced4dd7c29870aef543a 🚀

---
+++ assets/favorites-574ac0fa.js
@@ -0,0 +1,2 @@
+import"./refs-be07ecb5.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
+//# sourceMappingURL=favorites-574ac0fa.js.map
--- assets/favorites-6fd9b5c5.js
+++
@@ -1,2 +0,0 @@
-import"./refs-c1696e77.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
-//# sourceMappingURL=favorites-6fd9b5c5.js.map
