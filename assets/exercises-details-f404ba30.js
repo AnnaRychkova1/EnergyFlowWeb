11f296e74942f7a6f189f3198424ddc5e2e665d9
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-15 12:21:43 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@264375cabd70c6823514f0718cafe60ffcedbaca 🚀

---
+++ assets/exercises-details-f404ba30.js
@@ -0,0 +1,29 @@
+import{a as b}from"./vendor-8cce9181.js";import{hideLoader as c,hide as f,show as n,showLoader as g}from"./visibility-9f0213da.js";import{refs as e}from"./refs-be07ecb5.js";import d from"./isiToast-34d29aba.js";import{createModalMenu as y}from"./modal-menu-dfca6fcc.js";const E="https://energyflow.b.goit.study/api",C="exercises",o={filter:"",keyword:"",page:1,limit:9};window.innerWidth<=768?o.limit=8:o.limit=9;async function w(s,t){c(e.loaderModal);let i;if(s==="Body parts"?i="bodypart":s==="Muscles"?i="muscles":s==="Equipment"&&(i="equipment"),e.exercisesGalleryEl&&(f(e.subexercisesDetailsContainer),e.subexercisesSearchForm.reset()),e.subexercisesFilteredCards.innerHTML="",e.exercisesSubtitle.textContent=`${t}`,n(e.subexercisesDetailsContainer),n(e.subexercisesSearchForm),g(e.loaderModal),!i||!t){d.noResults(),n(e.subexercisesTextNoFound),c(e.loaderModal);return}try{const{results:r,totalPages:a}=await p({filter:i,name:t,keyword:o.keyword,limit:o.limit,page:o.page});if(console.log(a),console.log(r),console.log(o.page),a<1){d.noResults(),n(e.subexercisesTextNoFound),c(e.loaderModal);return}x(r),e.subExercisesPaginationContainer||h(a)}catch(r){console.error("Error fetching images:",r),d.apiIsiToastError()}finally{c(e.loaderModal),e.subexercisesSearchForm.reset()}//!  Works with search button
+e.subexercisesSearchForm.addEventListener("submit",l);async function l(r){r.preventDefault(),e.exercisesGalleryEl&&(f(e.subexercisesDetailsContainer),f(e.subexercisesSearchForm)),e.exercisesSubtitle.textContent=`${t}`,n(e.subexercisesDetailsContainer),n(e.subexercisesSearchForm),g(e.loaderModal),e.subexercisesFilteredCards.innerHTML="";const a=new FormData(r.target);if(o.keyword=a.get("query"),console.log(o.keyword),o.keyword.trim()===""){d.noQuery(),c(e.loaderModal);return}try{const{results:u,totalPages:m}=await p({filter:i,name:t,keyword:o.keyword,limit:o.limit,page:o.page});if(m<1){d.noResults(),n(e.subexercisesTextNoFound),c(e.loaderModal);return}m>=2&&h(m),x(u),o.page+=1}catch(u){console.error("Error fetching images:",u),d.apiIsiToastError()}finally{f(e.loaderModal)}}}e.subexercisesFilteredCards.addEventListener("click",M);function M(s){if(!s.target.dataset.id)return;const t=s.target.dataset.id;f(e.subexercisesDetailsContainer),y(t)}async function p({filter:s,name:t,keyword:i,limit:l,page:r}){return(await b.get(`${E}/${C}`,{params:{[s]:t,keyword:i,limit:l,page:r}})).data}function x(s){const t=s.map(i=>v(i)).join("");e.subexercisesFilteredCards.innerHTML=t}function v({_id:s,rating:t,name:i,burnedCalories:l,time:r,bodyPart:a,target:u}){return`<li class="filtered-card-item">
+        <div class="card-box-workout">
+          <div class="card-box-info">
+            <div class="filtered-workout">Workout</div>
+            <div class="card-box-rating">
+              <p class="filtered-rating">${Math.round(t)}</p>
+              <img class="filteered-star" href="#" alt="star" height="35"></img>
+            </div>
+          </div>
+          <a><button class="to-favorites-start" type="submit" data-id=${s} class="to-favorites-start">Start</button><a/>
+        </div>
+        <div class="card-box-title">
+          <img class="filtered-athlete" href="#" alt="athlete" height="35"></img>
+          <h3 class="filteered-title">${i}</h3>
+        </div>
+        <ul class="filtered-description">
+          <li class="filtered-descr-item">
+            <p class="filtered-descr-title">Burned calories: <spam class="filtered-descr-value">${l} / ${r} min</spam></p>
+          </li>
+          <li class="filtered-descr-item">
+            <p class="filtered-descr-title">Body part: <spam class="filtered-descr-value">${a}</spam></p>
+          </li>
+          <li class="filtered-descr-item">
+            <p class="filtered-descr-title">Target: <spam class="filtered-descr-value">${u}</spam></p>
+          </li>
+        </ul>
+  </li>`}function h(s,t){e.subExercisesPaginationContainer.innerHTML="";let i=Math.max(1,t-1),l=Math.min(s,i+2);console.log(t);for(let r=i;r<=l;r++){const a=document.createElement("button");a.textContent=r,a.classList.add("subexercises-pagination-button"),r===t&&a.classList.add("active"),a.addEventListener("click",()=>k(r)),e.subExercisesPaginationContainer.appendChild(a)}console.log(e.subExercisesPaginationContainer)}function k(s){o.page=s,console.log(s),w()}export{w as renderExerciseByFilterName};
+//# sourceMappingURL=exercises-details-f404ba30.js.map
--- assets/modal-menu-dfca6fcc.js
+++ assets/modal-menu-dfca6fcc.js
@@ -1,10 +1,10 @@
-import{a as g}from"./vendor-8cce9181.js";const v="/EnergyFlowWeb/assets/symbol-defs-251afd51.svg";document.querySelector(".results");const m=document.querySelector(".backdrop"),x=document.querySelector(".modal");document.querySelector(".ex-add-favorite");let o;async function y(s){o=s;try{let n=function(d){const a=JSON.parse(localStorage.getItem("favorites"))||[];if(console.log(a),a.some(t=>t._id===o)){const t=a.filter(l=>l._id!==o);localStorage.setItem("favorites",JSON.stringify(t)),element.classList.remove("is-favorite"),element.innerHTML=changingButtonName("add")}else localStorage.setItem("favorites",JSON.stringify([...a,e])),element.classList.add("is-favorite"),element.innerHTML=changingButtonName("remove")};const e=await f(o);console.log(e),h(e),document.querySelector(".ex-add-favorite").addEventListener("click",n),document.querySelector(".modal-close-btn").addEventListener("click",onCloseModal),m.classList.remove("is-hidden"),console.log(o)}catch(e){console.error("Error fetching images:",e)}}async function f(s){console.log(s);try{const e="https://energyflow.b.goit.study/api",i="exercises",{data:c}=await g.get(`${e}/${i}/${s}`);return c}catch(e){console.error(e)}}function h({_id:s,bodyPart:e,equipment:i,time:c,target:n,burnedCalories:d,gifUrl:a,name:r,popularity:t,rating:l,description:u}){const p=`<div class="modal-container">
+import{a as x}from"./vendor-8cce9181.js";const n="/EnergyFlowWeb/assets/symbol-defs-251afd51.svg";document.querySelector(".results");const g=document.querySelector(".backdrop"),h=document.querySelector(".modal");document.querySelector(".ex-add-favorite");let c;async function y(s){c=s;try{let t=function(o){console.log(o),console.log(c)};const e=await m(c);console.log(e),b(e),console.log(c),document.querySelector(".ex-add-favorite").addEventListener("click",t),g.classList.remove("is-hidden")}catch(e){console.error("Error fetching images:",e)}}async function m(s){console.log(s);try{const e="https://energyflow.b.goit.study/api",a="exercises",{data:t}=await x.get(`${e}/${a}/${s}`);return t}catch(e){console.error(e)}}function b({_id:s,bodyPart:e,equipment:a,time:t,target:o,burnedCalories:r,gifUrl:l,name:i,popularity:d,rating:u,description:p}){const v=`<div class="modal-container">
             <button class="modal-close-btn">
                 <svg
                     class="modal-close-icon"
                     width="24"
                     height="24">
-                    <use href="${v}#icon-x"></use>
+                    <use href="${n}#icon-x"></use>
                 </svg>
             </button>
             <div class="exercise-gif">
@@ -23,17 +23,17 @@ import{a as g}from"./vendor-8cce9181.js";const v="/EnergyFlowWeb/assets/symbol-d
                   />
                   <img
                     class="gif-ex"
-                    src="${a}"
+                    src="${l}"
                     width="295"
                     height="258"
-                    alt="${r}"
+                    alt="${i}"
                   />
               </picture>
             </div>
             <div class="ex-content-container">
-                <h3 class="exercise-name">${r}</h3>
+                <h3 class="exercise-name">${i}</h3>
                <div class="rating-container">
-                <p class="ex-current-rating">${l}</p>
+                <p class="ex-current-rating">${u}</p>
                 <ul class="exercise-stars-list">
                      
                 </ul>
@@ -41,7 +41,7 @@ import{a as g}from"./vendor-8cce9181.js";const v="/EnergyFlowWeb/assets/symbol-d
                 <div class="exercise-information">
                     <div class="ex-block">
                         <span class="exercise-value">Target</span>
-                        <span class="exercise-label">${n}</span>
+                        <span class="exercise-label">${o}</span>
                     </div>
                     <div class="ex-block">
                         <span class="exercise-value">Body part</span>
@@ -49,29 +49,29 @@ import{a as g}from"./vendor-8cce9181.js";const v="/EnergyFlowWeb/assets/symbol-d
                     </div>
                     <div class="ex-block">
                         <span class="exercise-value">Equipment</span>
-                        <span class="exercise-label">${i}</span>
+                        <span class="exercise-label">${a}</span>
                     </div>
                     <div class="ex-block">
                         <span class="exercise-value">Popular</span>
-                        <span class="exercise-label">${t}</span>
+                        <span class="exercise-label">${d}</span>
                     </div>
                     <div class="ex-block">
                         <span class="exercise-value">Burned calories</span>
-                        <span class="exercise-label">${d}/3 min</span>
+                        <span class="exercise-label">${r}/3 min</span>
                     </div>
 
                     <p class="exercise-description">
-                        ${u}
+                        ${p}
                     </p>
 
                     <div class="ex-add-btn-container">
-                        <button data-id="${s}" class="ex-add-favorite">
+                        <button type="button" data-id="${s}" class="ex-add-favorite">
                             Add to favorites
                             <svg
                                 class="heart-svg"
                                 width="18"
                                 height="18">
-                                <use href="${v}#icon-heart"></use>
+                                <use href="${n}#icon-heart"></use>
                             </svg>
                         </button>
                          <button data-id="${s}" class="ex-rating-button">
@@ -81,5 +81,5 @@ import{a as g}from"./vendor-8cce9181.js";const v="/EnergyFlowWeb/assets/symbol-d
                 </div>
             </div>
         </div>
-    `;x.innerHTML=p}document.querySelectorAll(".ex-rate-icon");export{y as createModalMenu};
-//# sourceMappingURL=modal-menu-afaba17c.js.map
+    `;h.innerHTML=v}document.querySelectorAll(".ex-rate-icon");export{y as createModalMenu};
+//# sourceMappingURL=modal-menu-dfca6fcc.js.map
