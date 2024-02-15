1b2964279f61837f6ed68bc423f8dd484c4cf151
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-02-15 16:54:41 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@cbde010b40e94078fc6d54d1c8b7c41e9e95a794 🚀

--- assets/favorites-574ac0fa.js
+++
@@ -1,2 +0,0 @@
-import"./refs-be07ecb5.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
-//# sourceMappingURL=favorites-574ac0fa.js.map
---
+++ assets/favorites-a83165a5.js
@@ -0,0 +1,2 @@
+import"./refs-000017b9.js";const o="quoteResponse",e=JSON.parse(localStorage.getItem(o));console.log(e);
+//# sourceMappingURL=favorites-a83165a5.js.map
