15aac5e06eb232f73d22598aebdb35a78378c183
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-02-16 06:53:52 +0000

    Deploying to gh-pages from @ AnnaRychkova1/EnergyFlowWeb@927516eb445348de749365dc38a04a268fb54b84 🚀

---
+++ assets/favorites-f572f91f.js
@@ -0,0 +1,2 @@
+const e=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"})),o=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"}));export{o as f,e as h};
+//# sourceMappingURL=favorites-f572f91f.js.map
