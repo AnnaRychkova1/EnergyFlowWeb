b6fb221e7e92d1322b36d20218f497e744451992
AnnaRychkova1 <aanytkaa@gmail.com>
2024-06-16 22:35:51 +0100

    refs

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -8,20 +8,13 @@ import { errorResult } from '/js/services/iziToast.js';
 const BASE_URL = 'https://energyflow.b.goit.study/api';
 const ENDPOINT_QUOTE = 'quote';
 
-// display quote
-function displayQuoteOnPage(quoteData) {
-  const quoteText = document.querySelector('.quote-text');
-  const quoteAuthor = document.querySelector('.quote-author');
-  quoteText.textContent = quoteData.quote;
-  quoteAuthor.textContent = quoteData.author;
-}
-
 getQuoteOffTheDay();
 
 async function getQuoteOffTheDay() {
   const quoteResponse = JSON.parse(localStorage.getItem('quoteResponse'));
   const savedDate = JSON.parse(localStorage.getItem('savedDate'));
   const currentDate = new Date();
+
   showLoader(refs.loaderModal);
   try {
     let quoteData;
@@ -33,18 +26,27 @@ async function getQuoteOffTheDay() {
       ) {
         // Використовуємо збережену цитату з локального сховища
         quoteData = quoteResponse;
+        displayQuoteOnPage(quoteData);
       }
-      // Отримуємо нову цитату з сервера
-      quoteData = await fetchQuoteFromServer();
-      saveQuoteToLocalStorage(quoteData);
     }
-    // Відображаємо цитату на сторінці
+
+    // Отримуємо нову цитату з сервера
+    quoteData = await fetchQuoteFromServer();
+    saveQuoteToLocalStorage(quoteData);
     displayQuoteOnPage(quoteData);
   } catch (error) {
     errorResult('Server Quotes did not respond');
   } finally {
     hideLoader(refs.loaderModal);
   }
+}
+
+// display quote
+function displayQuoteOnPage(quoteData) {
+  const quoteText = document.querySelector('.quote-text');
+  const quoteAuthor = document.querySelector('.quote-author');
+  quoteText.textContent = quoteData.quote;
+  quoteAuthor.textContent = quoteData.author;
 }
 
 // request to server
